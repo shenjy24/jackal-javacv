bfe0e707243ef75b2330e6b3c251d89cf4a71f25
shenjy <shenjy24@gmail.com>
2022-03-07 20:23:11 +0800

    按帧录制本机摄像头视频

--- src/main/java/com/jonas/javavc/CameraDemo.java
+++ src/main/java/com/jonas/javavc/CameraDemo.java
@@ -1,8 +1,8 @@
 package com.jonas.javavc;
 
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.ffmpeg.global.avcodec;
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.IplImage;
 
 import javax.swing.*;
 
@@ -10,12 +10,19 @@ public class CameraDemo {
 
     public static void main(String[] args) {
         try {
-            new CameraDemo().grab();
-        } catch (FrameGrabber.Exception e) {
+            CameraDemo app = new CameraDemo();
+            //new CameraDemo().grab();
+            app.record("output.mp4", 25);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * 调用本地摄像头
+     *
+     * @throws FrameGrabber.Exception
+     */
     public void grab() throws FrameGrabber.Exception {
         //新建opencv抓取器，一般的电脑和移动端设备中摄像头默认序号是0，不排除其他情况
         OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
@@ -30,5 +37,49 @@ public class CameraDemo {
             canvas.showImage(grabber.grab());
         }
         grabber.close();//停止抓取
+    }
+
+    /**
+     * 按帧录制本机摄像头视频（边预览边录制，停止预览即停止录制）
+     *
+     * @param outputFile  录制的文件路径，也可以是rtsp或者rtmp等流媒体服务器发布地址
+     * @param frameRate   视频帧率
+     */
+    public void record(String outputFile, double frameRate) throws FrameGrabber.Exception, FrameRecorder.Exception, InterruptedException {
+        FrameGrabber grabber = FrameGrabber.createDefault(0);
+        grabber.start();
+
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();//转换器
+        IplImage grabbedImage = converter.convert(grabber.grab());//抓取一帧视频并将其转换为图像
+        int width = grabbedImage.width();
+        int height = grabbedImage.height();
+
+        FrameRecorder recorder = FrameRecorder.createDefault(outputFile, width, height);
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264); // avcodec.AV_CODEC_ID_H264，编码
+        recorder.setFormat("flv");//封装格式，如果是推送到rtmp就必须是flv封装格式
+        recorder.setFrameRate(frameRate);
+        recorder.start();//开启录制器
+
+        long startTime = 0;
+        long videoTS = 0;
+        CanvasFrame frame = new CanvasFrame("camera", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(true);
+        Frame rotatedFrame = converter.convert(grabbedImage);
+
+        while (frame.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
+            rotatedFrame = converter.convert(grabbedImage);
+            frame.showImage(rotatedFrame);
+            if (startTime == 0) {
+                startTime = System.currentTimeMillis();
+            }
+            videoTS = 1000 * (System.currentTimeMillis() - startTime);
+            recorder.setTimestamp(videoTS);
+            recorder.record(rotatedFrame);
+            Thread.sleep(40);
+        }
+        frame.dispose();//关闭窗口
+        recorder.close();//关闭推流录制器，close包含release和stop操作
+        grabber.close();//关闭抓取器
     }
 }
